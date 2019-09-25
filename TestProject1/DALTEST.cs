0a892c726b91115e534920bb11625fc5dbbd906c
Unknown <alireza_esi14@yahoo.com>
2019-09-25 19:56:29 +0200

    some unit tests

---
+++ TestProject1/DALTEST.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using DAL;
+using BL.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class DALTEST
+    {
+        [TestMethod]
+        public void Login_Test_null_photgrapher()
+        {
+            int i = 1234;
+            byte[] b = BitConverter.GetBytes(i);
+            var login = new Login();
+            var person = login.authorization("xxx@xxx.com",b );
+            Assert.IsNull(person);
+        }
+        [TestMethod]
+        public void Login_Test()
+        {
+            string i ="Sina1371$$" ;
+            byte[] b = Encoding.ASCII.GetBytes(i);
+            var login = new Login();
+            BLPhotographer person = login.authorization("barakatisina@gmail.com",b );
+            Assert.IsNull(person);
+        }
+    }
+}
---
+++ TestProject1/MODELTEST.cs
@@ -0,0 +1,33 @@
+﻿using BL.Model;
+using BL.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class MODELTEST
+    {
+        [TestMethod]
+        public void BlExif_Test()
+        {
+            var model = new BLExif();
+            model.CameraMaker = "test";
+            Assert.IsNotNull(model.CameraMaker);
+        }
+        [TestMethod]
+        public void BlPhoto_Test()
+        {
+            var model = new BLPhoto();
+           
+            Assert.AreEqual(model.GetType(),typeof(BLPhoto));
+        }
+        [TestMethod]
+        public void Model_Types()
+        {
+            var model = new BLPhoto();
+            var model2 = new BLPhotographer();
+           
+            Assert.AreNotEqual(model.GetType(),model2.GetType());
+        }
+    }
+}
