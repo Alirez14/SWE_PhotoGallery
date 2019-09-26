61ca3067328fd815b08dd59451db589a27903baf
me barakati <barakatisina@gmail.com>
2019-09-26 11:52:08 +0200

    All Things Allright

--- TestProject1/DALTEST.cs
+++ TestProject1/DALTEST.cs
@@ -15,17 +15,19 @@ namespace TestProject1
             int i = 1234;
             byte[] b = BitConverter.GetBytes(i);
             var login = new Login();
-            var person = login.authorization("xxx@xxx.com",b );
+            var person = login.authorization("xxx@xxx.com", b);
             Assert.IsNull(person);
         }
+
         [TestMethod]
         public void Login_Test()
         {
-            string i ="Sina1371$$" ;
+            string i = "Sina1371$$";
             byte[] b = Encoding.ASCII.GetBytes(i);
             var login = new Login();
-            BLPhotographer person = login.authorization("barakatisina@gmail.com",b );
+            BLPhotographer person = login.authorization("barakatisina@gmail.com", b);
             Assert.IsNotNull(person);
         }
     }
 }
+
